5f2d76323fe4cbf36640990dc98f5ede53715d5b
Wesley Dusell <dusellfam@gmail.com>
2022-08-04 22:49:10 -0400

    rename modules to blocks

--- projects/ngx-editorjs/src/lib/ngx-editorjs.module.ts
+++ projects/ngx-editorjs/src/lib/ngx-editorjs.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 // import { ContenteditableValueAccessor } from './contenteditable-value-accessor/contenteditable-value-accessor.directive';
 import { NgxEditorjsComponent } from './ngx-editorjs.component';
-import { HeaderModule } from './modules/header/header.module';
+import { HeaderModule } from './blocks/header/header.module';
 import { BaseBlockComponent } from './base-block/base-block.component';
 
 
