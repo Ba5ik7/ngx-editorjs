6353819486145d2e5ad42d21b1f0d0f0c3839ef6
Wesley Dusell <dusellfam@gmail.com>
2022-08-09 12:09:29 -0400

    Setup mat button hover effect

--- projects/ngx-editorjs/src/lib/blocks/toolbar-block/toolbar-block.module.ts
+++ projects/ngx-editorjs/src/lib/blocks/toolbar-block/toolbar-block.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarBlockComponent } from './toolbar-block.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -9,7 +10,8 @@ import { ToolbarBlockComponent } from './toolbar-block.component';
     ToolbarBlockComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
   exports: [
     ToolbarBlockComponent
