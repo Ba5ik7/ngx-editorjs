9a3b5f4f27a434c7e62a6ac61bff7187fe7923c0
Wesley DuSell <dusellfam@gmail.com>
2022-09-06 14:29:30 -0400

    Create a directive that will autofocus a input field.

--- projects/ngx-editorjs/src/lib/directives/autofocus/autofocus.directive.ts
+++ projects/ngx-editorjs/src/lib/directives/autofocus/autofocus.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[libAutofocus]'
+  selector: '[autofocus]'
 })
-export class AutofocusDirective {
+export class AutofocusDirective implements OnInit {
+  _autofocus!: boolean;
+  @Input() set autofocus(condition: boolean) {
+    this._autofocus = condition != false;
+  }
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
+  ngOnInit() {
+    if (this._autofocus || typeof this._autofocus === 'undefined') {
+      this.elementRef.nativeElement.focus();      //For SSR (server side rendering) this is not safe. Use: https://github.com/angular/angular/issues/15008#issuecomment-285141070)
+    }
+  }
 }
