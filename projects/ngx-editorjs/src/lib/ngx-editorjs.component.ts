1a6bf05803fba2af6bb860a63996435669552591
Wesley DuSell <dusellfam@gmail.com>
2022-09-27 21:42:45 -0400

    Use mediator to add blocks to the DOM.

--- projects/ngx-editorjs/src/lib/ngx-editorjs.component.ts
+++ projects/ngx-editorjs/src/lib/ngx-editorjs.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CVAMediatorComponent, FormComponent } from './components/cvamediator/cvamediator.component';
 import { AdjustBlockPostionActions, NgxEditorjsService } from './ngx-editorjs.service';
 
 @Component({
@@ -10,12 +11,12 @@ import { AdjustBlockPostionActions, NgxEditorjsService } from './ngx-editorjs.se
 
 export class NgxEditorjsComponent implements OnInit {
 
+  @ViewChild('ngxEditor', { read: ViewContainerRef }) ngxEditor!: ViewContainerRef;
+
+  controlName: number = 0;
   formGroup: FormGroup = this.formBuilder.group({
-    init: ['', []],
-    one: ['', []],
-    two: ['', []],
-    three: ['', []]
-  })
+    [this.controlName.toString()]: this.formBuilder.control('', [])
+  });
 
   constructor(
     private formBuilder: FormBuilder,
@@ -29,6 +30,21 @@ export class NgxEditorjsComponent implements OnInit {
     .subscribe((direction: AdjustBlockPostionActions) => console.log({ direction }));
 
     this.ngxEditorjsService.addNewBlock$
-    .subscribe((block: any) => console.log({ block }));
+    .subscribe((block: any) => this.addNewBlock());
+  }
+
+  addNewBlock() {
+    // Add control to form group
+    this.controlName++;
+    this.formGroup.addControl(
+      this.controlName.toString(),
+      this.formBuilder.control('', [])
+    );
+    // Create component
+    const componentRef = this.ngxEditor.createComponent(CVAMediatorComponent);
+
+    const fieldComponent = componentRef.instance as FormComponent;
+    fieldComponent.formControlName = this.controlName.toString();
+    fieldComponent.form = this.formGroup;
   }
 }
