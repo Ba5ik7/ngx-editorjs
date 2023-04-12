a312f0053abfcdd75ca25771f27fea19c3c67108
Wesley DuSell <dusellfam@gmail.com>
2023-04-11 21:40:33 -0400

    Saving

--- projects/ngx-editorjs/src/lib/components/blocks/base-block-mediator/base-block-mediator.component.ts
+++ projects/ngx-editorjs/src/lib/components/blocks/base-block-mediator/base-block-mediator.component.ts
@@ -1,18 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'lib-base-block-mediator',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <p>
-      base-block-mediator works!
-    </p>
-  `,
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
+@Component({ template: `` })
 export class BaseBlockMediatorComponent {
+
 }
