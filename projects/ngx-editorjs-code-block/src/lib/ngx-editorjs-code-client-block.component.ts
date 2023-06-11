13bf3dd899f661bdb258563abd5f0535e213d6a4
Wesley DuSell <dusellfam@gmail.com>
2023-06-11 01:51:01 -0400

    Poss bug fix for animation transform

--- projects/ngx-editorjs-code-block/src/lib/ngx-editorjs-code-client-block.component.ts
+++ projects/ngx-editorjs-code-block/src/lib/ngx-editorjs-code-client-block.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CodemirrorModule } from '@ctrl/ngx-codemirror';
@@ -5,15 +6,20 @@ import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 @Component({
   standalone: true,
   imports: [
+    CommonModule,
     CodemirrorModule,
     FormsModule
   ],
   selector: 'ngx-editorjs-code-client-block',
   template: `
-    <ngx-codemirror
-      [ngModel]="value"
-      [options]="codeMirrorOptions">
-    </ngx-codemirror>
+    <ng-container *ngIf="show">
+      <ngx-codemirror
+        class="code-editor"
+        [ngModel]="value"
+        [options]="codeMirrorOptions">
+      </ngx-codemirror>
+    </ng-container>
+    
   `,
   styles: [`
     :host {
@@ -24,6 +30,14 @@ import { CodemirrorModule } from '@ctrl/ngx-codemirror';
       position: relative;
       z-index: 1;
     }
+    @keyframes fade {
+      from { opacity: 0; }
+      to { opacity: 1; }
+    }
+    .code-editor {
+      animation: fade 1s;
+    }
+    
   `]
 })
 export class NgxEditorjsCodeClientBlockComponent {
@@ -40,5 +54,13 @@ export class NgxEditorjsCodeClientBlockComponent {
   @Input() 
   set savedAction(action: string) {
     this.codeMirrorOptions.mode = action;
+  }
+
+  show = false;
+  ngOnInit() {
+    // Parent component transfrom animations causes the code editor
+    // to be longer. Because the height is skewed. This is a
+    // workaround to fix that.
+    setTimeout(() => this.show = true, 1000);
   }
 }
